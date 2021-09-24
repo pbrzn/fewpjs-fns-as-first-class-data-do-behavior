1839103d05101131cc158c4016954338b0654e20
Patrick Brennan <pbrennanmusic@gmail.com>
2021-09-24 12:04:08 -0400

    commit tree 772d246ba9064ac78d8ae1576bc6ea98413a7caa

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(timeString) {
+  const timeInts = timeString.split(':');
+  const hour = parseInt(timeInts[0]);
+  if (hour < 12) {
+    return "Good Morning";
+  }
+  else if (hour >= 12 && hour < 17) {
+    return "Good Afternoon";
+  }
+  else {
+    return "Good Evening";
+  }
+}
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  const greeting = document.querySelector('h1#greeting')
+  greeting.innerHTML = string
+}
